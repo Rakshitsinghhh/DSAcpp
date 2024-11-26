4198803d859788bc0a8be319f437f0cddfd04d9f
Rakshitsinghhh <singhrakshit2404@gmail.com>
2024-11-26 23:34:45 +0530

    Add files via upload

---
+++ 111leet.cpp
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int minDepth(TreeNode* root) {
+        if (root == nullptr) {
+            return 0;
+        }
+        int r = minDepth(root->left);
+        int l = minDepth(root->right);
+        if (root->left == nullptr || root->right == nullptr) {
+            return 1 + l + r;
+        }
+
+        return 1 + min(r, l);
+    }
+};
